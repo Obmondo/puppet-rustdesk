28f2d6d19187d1e6f526f728f7cc8fa22a5cfeac
Sidharth Jawale <sidharth@obmondo.com>
2026-09-22 04:01:10 +0530

    feat: add rspec unit tests for rustdesk module
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

---
+++ spec/classes/rustdesk_server_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe 'rustdesk::server' do
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+
+      context 'when enabled' do
+        let(:params) do
+          {
+            'enable' => true,
+            'version' => '1.7.2',
+            'extra_dependencies' => [],
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_package('rustdesk-server-hbbs') }
+        it { is_expected.to contain_package('rustdesk-server-hbbr') }
+        it { is_expected.to contain_service('hbbs') }
+        it { is_expected.to contain_service('hbbr') }
+      end
+
+      context 'when disabled' do
+        let(:params) do
+          {
+            'enable' => false,
+            'version' => '1.7.2',
+            'extra_dependencies' => [],
+          }
+        end
+
+        it { is_expected.to compile }
+        it { is_expected.to contain_package('rustdesk-server-hbbs').with_ensure('absent') }
+        it { is_expected.to contain_package('rustdesk-server-hbbr').with_ensure('absent') }
+      end
+    end
+  end
+end
