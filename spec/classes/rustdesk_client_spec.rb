4721507fdfa659386982c90c014d33643d702409
Sidharth Jawale <sidharth@obmondo.com>
2026-09-22 04:07:23 +0530

    chore: update rustdesk client package version to 1.4.9
    Signed-off-by: Sidharth Jawale <sidharth@obmondo.com>

--- spec/classes/rustdesk_client_spec.rb
+++ spec/classes/rustdesk_client_spec.rb
@@ -9,14 +9,14 @@ describe 'rustdesk::client' do
         let(:params) do
           {
             'enable' => true,
-            'version' => '1.4.4',
+            'version' => '1.4.9',
             'extra_dependencies' => [],
           }
         end
 
         it { is_expected.to compile }
         it { is_expected.to contain_package('libxcb-randr0') }
-        it { is_expected.to contain_archive('/tmp/rustdesk-1.4.4-x86_64.deb') }
+        it { is_expected.to contain_archive('/tmp/rustdesk-1.4.9-x86_64.deb') }
         it { is_expected.to contain_package('rustdesk') }
         it { is_expected.to contain_service('rustdesk') }
       end
@@ -25,7 +25,7 @@ describe 'rustdesk::client' do
         let(:params) do
           {
             'enable' => false,
-            'version' => '1.4.4',
+            'version' => '1.4.9',
             'extra_dependencies' => [],
           }
         end
